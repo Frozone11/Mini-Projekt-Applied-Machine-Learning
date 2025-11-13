8ad04f683cd6fd7df5768f8487bb75d9652923f8
Frozone11 <hjalte_lillevik@live.dk>
2025-11-13 21:18:20 +0100

    Create the Dataset "df"

--- AMMini.py
+++ AMMini.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
-# Read names from Faults27x7_var
-with open("Faults27x7_var", "r") as f:
-    names = [line.strip() for line in f.readlines()]
 
-print(len(names))
+with open("/Users/hjaltefrost/Library/Mobile Documents/com~apple~CloudDocs/Robotteknologi/7. semester/Applied Machine Learning/mini/steel+plates+faults/Faults27x7_var", "r") as f:
+    columns= [line.strip() for line in f.readlines()]
+
+print(len(columns))
+
+df = pd.read_csv(
+    "/Users/hjaltefrost/Library/Mobile Documents/com~apple~CloudDocs/Robotteknologi/7. semester/Applied Machine Learning/mini/steel+plates+faults/Faults.NNA",
+    sep=r"\s+",      # split on any whitespace (tabs/spaces)
+    header=None,
+    names=columns
+)
+
+
+print(df.shape)
+print(df.head())
