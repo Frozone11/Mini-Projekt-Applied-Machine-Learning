b9d38d8e63b72df7c264187dac7d8dfbba39ce6c
Frozone11 <hjalte_lillevik@live.dk>
2025-11-15 20:47:08 +0100

    ?????

--- AMMini.py
+++ AMMini.py
@@ -1,10 +1,10 @@
 
 #import numpy as np
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-#from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 #from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from ucimlrepo import fetch_ucirepo 
 
@@ -42,13 +42,22 @@ print(y_fault.value_counts())
 # We can now se the 7 fault collumns have been combined into one
 
 # Make train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y_fault, test_size=0.2, random_state=42,)
+X_train, X_test, y_train, y_test = train_test_split(X, y_fault, test_size=0.25, random_state=42,)
 
 # Scale the train and test features
 X_train_scaled = StandardScaler().fit_transform(X_train)
 X_test_scaled = StandardScaler().fit_transform(X_test)
 
 print("\nX_train_scaled shape:", X_train_scaled.shape) # See the shape of the scaled train features 
+
+# Here I create and fit the logistic regression model
+LogReg_model = LogisticRegression(random_state=42)
+LogReg_model.fit(X_train_scaled, y_train)
+
+# Prediction for the model and evaluation 
+y_predict = LogReg_model.predict(X_test_scaled)
+accuracy = LogReg_model.score(y_test, y_predict)
+print("\n Model accuracy:", accuracy)
 
 
 
